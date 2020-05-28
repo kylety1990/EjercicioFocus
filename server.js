ae8a0524dcf10c7b459bcdeea1473e8757233d36
Christian <cristian001_13@hotmail.com>
2020-05-28 22:41:25 +0200

    Finish

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+
 const express = require ('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 
-app.use(express.static(__dirname + '/dist'))
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(cors());
 
+app.use(function (req,ers,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "DELETE,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+app.use(express.static('www'))
 app.listen(process.env.PORT || 8080);
 
 app.get('/*' , function(req, res) {
